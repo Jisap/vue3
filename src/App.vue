81e3936c36b5f5870dd8e615324db2db852c975a
Jisap <67540940+Jisap@users.noreply.github.com>
2020-08-30 22:03:28 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <p>El contador marca: N</p>
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/composition-api";
+
+export default {
+  name: "App",
+  setup() {
+    const contador = ref(0);
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
